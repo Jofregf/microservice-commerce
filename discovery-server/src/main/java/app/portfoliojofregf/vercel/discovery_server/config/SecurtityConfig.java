2ab6ff7b3fa386f7b909058ac4a674f83f5cfabe
Guillermo Jofre <jofregf@gmail.com>
2024-03-14 22:18:06 -0300

    feat: KeyCloak
    Configuracion de keycloak en los distintos microservicios y creacion de los securityconfig necesarios.

---
+++ discovery-server/src/main/java/app/portfoliojofregf/vercel/discovery_server/config/SecurtityConfig.java
@@ -0,0 +1,16 @@
+package app.portfoliojofregf.vercel.discovery_server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurtityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+}
